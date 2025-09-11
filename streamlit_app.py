93f979614f5022713a32cfe69dde09be5dd73d25
Daniel Amado <64038165+danielAcl9@users.noreply.github.com>
2025-09-11 16:55:53 +0000

    Initial tests

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("Las 48 Leyes del Poder")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Oprime el botón para seleccionar aleatoriamiente una de las leyes para repasar el día de hoy."
 )
