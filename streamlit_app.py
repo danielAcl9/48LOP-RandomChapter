bdf7f40308b4916a89532b4e02d57591a79171bb
Daniel Amado <64038165+danielAcl9@users.noreply.github.com>
2025-09-11 17:16:04 +0000

    -

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,8 +6,6 @@ st.title("Las 48 Leyes del Poder")
 st.write(
     "Oprime el botón para seleccionar aleatoriamiente una de las leyes para repasar el día de hoy.")
 
-st.button("Generar", type = "primary")
-
-num = rd.randint(0, 9)
-
-st.write(randomChapter(num))
+if st.button("Generar", type="primary"):
+    num = rd.randint(0, 9)  # de 0 a 47 (48 leyes)
+    st.write(randomChapter(num))
