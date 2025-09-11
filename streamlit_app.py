4ed16e29dacb8bc70da0a7400d870122cf568828
Daniel Amado <64038165+danielAcl9@users.noreply.github.com>
2025-09-11 17:26:31 +0000

    Fixed bugs

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,21 +4,23 @@ from lists import randomChapter
 
 st.set_page_config(page_title="L48LdP", layout="centered")
 
-st.title("Las 48 Leyes del Poder")
-st.write(
-    "Oprime el botón para seleccionar aleatoriamiente una de las leyes para repasar el día de hoy.")
+with st.container(horizontal_alignment="center"):
 
-if st.button("Generar", type="primary"):
-    num, titulo = randomChapter(rd.randint(0, 9))
-    
-    # Número de la ley (azul y grande)
-    st.markdown(
-        f"<h2 style='text-align: center; color: royalblue;'>Ley {num}</h2>",
-        unsafe_allow_html=True
-    )
-    
-    # Título de la ley (debajo, centrado)
-    st.markdown(
-        f"<p style='text-align: center; font-size:18px;'>{titulo}</p>",
-        unsafe_allow_html=True
-    )
+    st.title("Las 48 Leyes del Poder")
+    st.write(
+        "Oprime el botón para seleccionar aleatoriamiente una de las leyes para repasar el día de hoy.")
+
+    if st.button("Generar", type="primary"):
+        num, titulo = randomChapter(rd.randint(0, 9))
+        
+        # Número de la ley (azul y grande)
+        st.markdown(
+            f"<h2 style='text-align: center; color: royalblue;'>Ley {num}</h2>",
+            unsafe_allow_html=True
+        )
+        
+        # Título de la ley (debajo, centrado)
+        st.markdown(
+            f"<p style='text-align: center; font-size:18px;'>{titulo}</p>",
+            unsafe_allow_html=True
+        )
