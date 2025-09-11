801f784d92a6afca67726c96af7a4d43853ce82f
Daniel Amado <64038165+danielAcl9@users.noreply.github.com>
2025-09-11 17:13:22 +0000

    Added generate button

---
+++ lists.py
@@ -0,0 +1,16 @@
+def randomChapter(chapter):
+    titles = [
+        "Nunca le haga sombra a su amo",
+        "Nunca confíe demasiado en sus amigos, aprenda a usar a sus enemigos",
+        "Disimule sus intenciones",
+        "Diga siempre menos de lo necesario",
+        "Casi todo depende de su prestigio, defiéndalo a muerte",
+        "Busque llamar la atención a cualquier precio",
+        "Logre que otros trabajen por usted, pero nu deje nunca de llevarse los laureles",
+        "Haga que la gente vaya hacia usted y, de ser necesario, utilice la carnada más adecuada para lograrlo",
+        "Gane a través de sus acciones, nunca por medio de argumentos",
+        "Peligro de contagio: Evite a los perdedores y desdichados"
+        # Ley 11 sigue
+    ]
+
+    return f"Ley {chapter + 1}: {titles[chapter]}"
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+import random as rd
+from lists import randomChapter
 
 st.title("Las 48 Leyes del Poder")
 st.write(
-    "Oprime el botón para seleccionar aleatoriamiente una de las leyes para repasar el día de hoy."
-)
+    "Oprime el botón para seleccionar aleatoriamiente una de las leyes para repasar el día de hoy.")
+
+st.button("Generar", type = "primary")
+
+num = rd.randint(0, 9)
+
+st.write(randomChapter(num))
