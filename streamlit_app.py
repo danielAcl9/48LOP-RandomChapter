e29b6fe1d849d29ca0547bc0a98d80a50d3f081f
Daniel Amado <64038165+danielAcl9@users.noreply.github.com>
2025-09-11 17:29:42 +0000

    Centered all content

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,23 +4,30 @@ from lists import randomChapter
 
 st.set_page_config(page_title="L48LdP", layout="centered")
 
-with st.container(horizontal_alignment="center"):
+# Título centrado con parte en azul
+st.markdown(
+    "<h1 style='text-align: center;'>Las <span style='color: royalblue;'>48</span> Leyes del Poder</h1>",
+    unsafe_allow_html=True
+)
 
-    st.title("Las 48 Leyes del Poder")
-    st.write(
-        "Oprime el botón para seleccionar aleatoriamiente una de las leyes para repasar el día de hoy.")
+# Subtítulo centrado
+st.markdown(
+    "<p style='text-align: center;'>Oprime el botón para seleccionar aleatoriamente una de las leyes para repasar el día de hoy.</p>",
+    unsafe_allow_html=True
+)
 
-    if st.button("Generar", type="primary"):
-        num, titulo = randomChapter(rd.randint(0, 9))
-        
-        # Número de la ley (azul y grande)
-        st.markdown(
-            f"<h2 style='text-align: center; color: royalblue;'>Ley {num}</h2>",
-            unsafe_allow_html=True
-        )
-        
-        # Título de la ley (debajo, centrado)
-        st.markdown(
-            f"<p style='text-align: center; font-size:18px;'>{titulo}</p>",
-            unsafe_allow_html=True
-        )
+# Botón y resultado
+if st.button("Generar", type="primary"):
+    num, titulo = randomChapter(rd.randint(0, 47))
+
+    # Número de la ley (azul y grande)
+    st.markdown(
+        f"<h2 style='text-align: center; color: royalblue;'>Ley {num}</h2>",
+        unsafe_allow_html=True
+    )
+
+    # Título de la ley (debajo, centrado)
+    st.markdown(
+        f"<p style='text-align: center; font-size:18px;'>{titulo}</p>",
+        unsafe_allow_html=True
+    )
